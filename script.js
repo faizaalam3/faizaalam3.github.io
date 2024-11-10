cfb47862887aa9f9ce1b8ff066ea74e302b68aa1
faizaalam3 <fi.faizaalam@gmail.com>
2024-11-10 21:43:20 +0500

    Fixed errors

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-let slideIndex = 0;
-showSlides(slideIndex);
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let slides = document.getElementsByClassName("slide");
-    let dots = document.getElementsByClassName("dot");
-    if (n >= slides.length) { slideIndex = 0 }
-    if (n < 0) { slideIndex = slides.length - 1 }
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (let i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex].style.display = "block";
-    dots[slideIndex].className += " active";
-}
-
-setInterval(() => {
-    showSlides(slideIndex += 1);
-}, 5000);
-
 // Smooth scrolling for anchor links
 function enableSmoothScrolling() {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
